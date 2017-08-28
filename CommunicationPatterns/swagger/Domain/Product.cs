ba4b76bfa6a9a5e9bcdba9ab8852d81f64ddd987
tamir.dresher@gmail.com <tamir.dresher@gmail.com>
2017-08-28 20:00:44 +0300

    swagger

---
+++ CommunicationPatterns/swagger/Domain/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace swagger.Controllers
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
