ea406861fcf6fd3c4e747fb7730911eeec30a9cf
tamir.dresher@gmail.com <tamir.dresher@gmail.com>
2017-08-31 00:42:34 +0300

    Retry and Circuit Breaker with Polly

---
+++ CircuitBreakerWithPolly/Program.cs
@@ -0,0 +1,48 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitBreakerWithPolly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CircuitBreakerPolicy breaker = Policy
+                .Handle<TimeoutException>()
+                .CircuitBreaker(2, TimeSpan.FromMinutes(1),
+                (exception, timespan, context) => { Console.WriteLine("OnBreak"); },
+                context => { Console.WriteLine("OnReset"); });
+
+            Console.WriteLine("---- Try 1 ----");
+            TryExecutingRiskyOperation(breaker);
+            Console.WriteLine("---- Try 2 ----");
+            TryExecutingRiskyOperation(breaker);
+            Console.WriteLine("---- Try 3 ----");
+            TryExecutingRiskyOperation(breaker);
+            Console.WriteLine("---- Try 4 ----");
+            TryExecutingRiskyOperation(breaker);
+
+        }
+        static void TryExecutingRiskyOperation(CircuitBreakerPolicy breaker)
+        {
+            try
+            {
+                breaker.Execute(() => { CloudOperation(); });
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("TryExecutingRiskyOperation - got exception");
+            }
+        }
+        private static void CloudOperation()
+        {
+            Console.WriteLine("Executing CloudOperation");
+            throw new TimeoutException("Server didnt respond");
+        }
+    }
+}
---
+++ RetryWithPolly/Program.cs
@@ -0,0 +1,50 @@
+﻿using Polly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetryWithPolly
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var logger = new Logger();
+            var policy = Policy
+                            .Handle<TimeoutException>()
+                            .WaitAndRetry(Enumerable.Repeat(TimeSpan.FromSeconds(5), 3),
+                            (exception, timespan) =>
+                            {
+                                logger.Warn($"{exception} trying again in {timespan}");
+                            });
+
+            PolicyResult result = policy.ExecuteAndCapture(() => TransientOperation());
+
+        }
+
+        static int attempt = 0;
+        private static void TransientOperation()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Executing the TransientOperation");
+            if (++attempt==3)
+            {
+                Console.WriteLine("SUCCESS");
+
+                return;
+            }
+            throw new TimeoutException("Server didnt repond");
+        }
+    }
+
+    class Logger
+    {
+        public void Warn(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
